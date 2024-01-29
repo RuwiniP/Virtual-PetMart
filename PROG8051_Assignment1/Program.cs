f134461dd7ad6074f87414bcec70d5aae96f3b7d
perer <perer@DESKTOP-7KGC5QU>
2024-01-28 23:09:51 -0500

    Add project files.

---
+++ PROG8051_Assignment1/Program.cs
@@ -0,0 +1,83 @@
+﻿using System.Linq.Expressions;
+
+namespace PROG8051_Assignment1
+{
+
+    class VirtualPet
+    {
+        static void Main(string[] args)
+
+        {
+            //List of pet types
+            List<string> pets = new List<string>(3);
+            pets.Add("1. Dog ");
+            pets.Add("2. Cat ");
+            pets.Add("3. Rabbit ");
+
+
+            //Display a welcome message
+            Console.WriteLine("---------Welcome to your virtual Pet Mart--------");
+            Console.WriteLine();
+
+            try{
+                //Take iput from user to identify the type of the pet
+                Console.WriteLine("Please choose a type of pet from the below list.");
+                foreach (string pet in pets)
+                    Console.WriteLine(pet);
+
+                Console.WriteLine();
+                Console.Write(" User Input: ");
+                int petType = Convert.ToInt32(Console.ReadLine());
+
+                //Validating the input
+                if (petType == 1)
+                {
+                    Console.WriteLine("You've chosen a Dog. What would you like to name your pet?");
+                    Console.Write(" User Input: ");
+                    string petName = Console.ReadLine();
+                    Console.WriteLine();
+                    Console.WriteLine("Welcome, " +petName+". Let's take good care of your lovely pet.");
+
+                }
+
+
+                else if (petType == 2)
+                {
+
+                    Console.WriteLine("You've chosen a Cat. What would you like to name your pet?");
+                    Console.Write(" User Input: ");
+                    string petName = Console.ReadLine();
+                    Console.WriteLine();
+                    Console.WriteLine("Welcome, " + petName + ". Let's take good care of your lovely pet.");
+                }
+
+
+                else if (petType == 3)
+                {
+
+                    Console.WriteLine("You've chosen a Rabbit. What would you like to name your pet?");
+                    Console.Write(" User Input: ");
+                    string petName = Console.ReadLine();
+                    Console.WriteLine();
+                    Console.WriteLine("Welcome, " + petName + ". Let's take good care of your lovely pet.");
+                }
+
+
+
+                else
+                {
+
+                    Console.WriteLine("SORRY!.Please input a valid petType from the list.");
+                }
+            }
+
+            catch(FormatException)
+            {
+                Console.WriteLine("INVALID NUMBER!. Please input a NUMBER from the list to proceed.");
+            }
+         }
+
+
+        
+    }
+}
